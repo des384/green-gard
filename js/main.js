913d094d1c872deed84aa6b1694b73320fdf4d1e
des384 <75841716+des384@users.noreply.github.com>
2021-02-14 16:04:11 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,3 +2,31 @@ $('.menu__btn').click(function (event) {
     $('.menu__btn,.menu').toggleClass('active');
     $('body').toggleClass('lock');
 });
+
+$(document).ready(function(){
+    $('.slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>',
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        responsive: [
+            {
+            breakpoint: 1175,
+            settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            arrows: true,
+            }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: true,
+        }
+    }
+        ]
+    });
+  });
